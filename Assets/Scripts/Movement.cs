52c6bcd9fecaa2469aaf8b26d590d21858c63230
Oene Bakker <oene.bakker@knid.nl>
2021-12-10 14:00:44 +0100

    Initial commmit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainTrust = 1000f;
+    [SerializeField] float rotationTrust = 100f;
+    Rigidbody rocketRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        rocketRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTrust();
+        ProcessRotation();
+    }
+
+    // Handle keyboard input: space
+    void ProcessTrust() {
+        if(Input.GetKey(KeyCode.Space)) {
+            rocketRigidBody.AddRelativeForce(Vector3.up * mainTrust * Time.deltaTime);
+        }
+    }
+
+    // Handle keyboard input: A and D
+    void ProcessRotation() {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationTrust);
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            ApplyRotation(-rotationTrust);
+        }
+    }
+
+    private void ApplyRotation(float trust)
+    {
+        // Freeze rotation so we can do manually rotation
+        // Otherwise the fyscics system can take over
+        rocketRigidBody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * trust * Time.deltaTime);
+        rocketRigidBody.freezeRotation = false;
+    }
+}
